c6c3b9f1620e1f82e9c51234bb568452c7512d00
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-11 16:37:15 -0400

    Wire Postgres resource into AppHost; verified end-to-end (container + gridpulsedb created)

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume();
+
+var gridpulseDb = postgres.AddDatabase("gridpulsedb");
+
 builder.Build().Run();
