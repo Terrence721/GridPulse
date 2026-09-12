b962aae80dcbf58ad0ebded0a8e797ee87af7e43
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-12 05:48:56 -0400

    Register MeterSimulatorOptions and MeterReadingGenerator in DI

--- src/MeterSimulator/Program.cs
+++ src/MeterSimulator/Program.cs
@@ -2,6 +2,9 @@ using GridPulse.MeterSimulator;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.AddServiceDefaults();
+builder.Services.Configure<MeterSimulatorOptions>(
+    builder.Configuration.GetSection(MeterSimulatorOptions.SectionName));
+builder.Services.AddSingleton<MeterReadingGenerator>();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
