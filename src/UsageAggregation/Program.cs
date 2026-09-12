5da55075382c981edd54fdde99c7885b80574b65
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-12 07:58:49 -0400

    Add POST /readings endpoint

--- src/UsageAggregation/Program.cs
+++ src/UsageAggregation/Program.cs
@@ -4,9 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.AddNpgsqlDbContext<UsageAggregationDbContext>("gridpulsedb");
+builder.Services.AddScoped<ReadingProcessor>();
 
 var app = builder.Build();
 app.MapDefaultEndpoints();
+
+app.MapPost("/readings", async (MeterReadingRequest request, ReadingProcessor processor, CancellationToken cancellationToken) =>
+{
+    var processed = await processor.ProcessAsync(request, cancellationToken);
+    return processed
+        ? Results.Accepted(value: new { status = "processed" })
+        : Results.Ok(new { status = "duplicate" });
+});
 
 using (var scope = app.Services.CreateScope())
 {
