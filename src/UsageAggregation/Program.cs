57b1b741465732350e68727ba379f7a6ae33518c
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-12 07:55:05 -0400

    Apply EF Core migrations at startup

--- src/UsageAggregation/Program.cs
+++ src/UsageAggregation/Program.cs
@@ -1,4 +1,5 @@
 using GridPulse.UsageAggregation;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
@@ -6,5 +7,11 @@ builder.AddNpgsqlDbContext<UsageAggregationDbContext>("gridpulsedb");
 
 var app = builder.Build();
 app.MapDefaultEndpoints();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<UsageAggregationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
