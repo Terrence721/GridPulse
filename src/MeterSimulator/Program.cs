537958e52de45c66a7f698e6267b7bd1d9f22def
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-11 23:56:06 -0400

    Call AddServiceDefaults() in Meter Simulator

--- src/MeterSimulator/Program.cs
+++ src/MeterSimulator/Program.cs
@@ -1,6 +1,7 @@
 using GridPulse.MeterSimulator;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
