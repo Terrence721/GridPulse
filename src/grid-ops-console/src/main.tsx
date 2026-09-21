da9d37b85681aa0c45d1855bdea876d1f46b6037
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-21 11:05:34 -0400

    Add: wire BrowserRouter into grid-ops-console

--- src/grid-ops-console/src/main.tsx
+++ src/grid-ops-console/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { AuthProvider } from 'react-oidc-context'
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.ts'
 import { oidcConfig } from './auth/oidcConfig.ts'
 import App from './App.tsx'
@@ -15,7 +16,9 @@ createRoot(document.getElementById('root')!).render(
       }}
     >
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Provider>
     </AuthProvider>
   </StrictMode>,
