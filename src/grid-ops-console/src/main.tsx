8ddd89e217a39d4257592c83fb3b02916537e4d1
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-21 09:40:02 -0400

    Add: wire the OIDC AuthProvider into grid-ops-console

--- src/grid-ops-console/src/main.tsx
+++ src/grid-ops-console/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { AuthProvider } from 'react-oidc-context'
 import { store } from './app/store.ts'
+import { oidcConfig } from './auth/oidcConfig.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <AuthProvider
+      {...oidcConfig}
+      onSigninCallback={() => {
+        window.history.replaceState({}, document.title, window.location.pathname)
+      }}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
   </StrictMode>,
 )
